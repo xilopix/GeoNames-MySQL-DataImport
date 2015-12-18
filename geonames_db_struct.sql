e12ca3483e8f8f994095b0d829e41a9db430ad08
paul <paul.bedaride@xilopix.com>
2015-12-18 15:28:07 +0100

    Improve sql schema

--- geonames_db_struct.sql
+++ geonames_db_struct.sql
@@ -4,50 +4,43 @@
 
 -- Create syntax for TABLE 'admin1CodesAscii'
 CREATE TABLE `admin1CodesAscii` (
-  `code` char(15) DEFAULT NULL,
-  `name` text,
-  `nameAscii` text,
-  `geonameid` int(11) DEFAULT NULL,
-  KEY `code` (`code`),
-  KEY `name` (`name`(20)),
-  KEY `nameAscii` (`nameAscii`(20)),
-  KEY `geonameid` (`geonameid`)
+  `code` char(15) NOT NULL,
+  `name` varchar(128),
+  `nameAscii` varchar(128),
+  `geonameid` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- Create syntax for TABLE 'admin2Codes'
 CREATE TABLE `admin2Codes` (
-  `code` char(15) DEFAULT NULL,
-  `name` text,
-  `nameAscii` text,
-  `geonameid` int(11) DEFAULT NULL,
-  KEY `code` (`code`),
-  KEY `name` (`name`(80)),
-  KEY `nameAscii` (`nameAscii`(80)),
-  KEY `geonameid` (`geonameid`)
+  `code` char(15) NOT NULL,
+  `name` varchar(128),
+  `nameAscii` varchar(128),
+  `geonameid` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- Create syntax for TABLE 'alternatename'
 CREATE TABLE `alternatename` (
-  `alternatenameId` int(11) NOT NULL,
-  `geonameid` int(11) DEFAULT NULL,
+  `alternatenameId` mediumint(8) UNSIGNED NOT NULL,
+  `geonameid` mediumint(8) UNSIGNED NOT NULL,
   `isoLanguage` varchar(7) DEFAULT NULL,
-  `alternateName` varchar(200) DEFAULT NULL,
+  `alternateName` varchar(256) DEFAULT NULL,
   `isPreferredName` tinyint(1) DEFAULT NULL,
   `isShortName` tinyint(1) DEFAULT NULL,
   `isColloquial` tinyint(1) DEFAULT NULL,
   `isHistoric` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`alternatenameId`),
-  KEY `geonameid` (`geonameid`),
   KEY `isoLanguage` (`isoLanguage`),
   KEY `alternateName` (`alternateName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- Create syntax for TABLE 'continentCodes'
 CREATE TABLE `continentCodes` (
-  `code` char(2) DEFAULT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `geonameid` int(11) DEFAULT NULL,
-  KEY `code` (`code`),
+  `code` char(2) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `geonameid` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`code`),
   KEY `name` (`name`),
   KEY `geonameid` (`geonameid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
@@ -69,11 +62,11 @@ CREATE TABLE `countryinfo` (
   `Phone` char(10) DEFAULT NULL,
   `postalCodeFormat` varchar(100) DEFAULT NULL,
   `postalCodeRegex` varchar(255) DEFAULT NULL,
-  `geonameId` int(11) DEFAULT NULL,
+  `geonameid` mediumint(8) UNSIGNED,
   `languages` varchar(200) DEFAULT NULL,
   `neighbours` char(100) DEFAULT NULL,
   `equivalentFipsCode` char(10) DEFAULT NULL,
-  KEY `iso_alpha2` (`iso_alpha2`),
+  PRIMARY KEY `iso_alpha2` (`iso_alpha2`),
   KEY `iso_alpha3` (`iso_alpha3`),
   KEY `iso_numeric` (`iso_numeric`),
   KEY `fips_code` (`fips_code`),
@@ -82,16 +75,16 @@ CREATE TABLE `countryinfo` (
 
 -- Create syntax for TABLE 'featureCodes'
 CREATE TABLE `featureCodes` (
-  `code` char(7) DEFAULT NULL,
+  `code` char(7) NOT NULL,
   `name` varchar(200) DEFAULT NULL,
-  `description` text,
-  KEY `code` (`code`),
+  `description` tinytext,
+  PRIMARY KEY `code` (`code`),
   KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- Create syntax for TABLE 'geoname'
 CREATE TABLE `geoname` (
-  `geonameid` int(11) NOT NULL,
+  `geonameid` MEDIUMINT(8) UNSIGNED NOT NULL,
   `name` varchar(200) DEFAULT NULL,
   `asciiname` varchar(200) DEFAULT NULL,
   `alternatenames` varchar(4000) DEFAULT NULL,
@@ -111,18 +104,13 @@ CREATE TABLE `geoname` (
   `timezone` varchar(40) DEFAULT NULL,
   `moddate` date DEFAULT NULL,
   PRIMARY KEY (`geonameid`),
-  KEY `name` (`name`),
+  KEY `name` (`name`)
   KEY `asciiname` (`asciiname`),
-  KEY `latitude` (`latitude`),
-  KEY `longitude` (`longitude`),
   KEY `fclass` (`fclass`),
   KEY `fcode` (`fcode`),
   KEY `country` (`country`),
   KEY `cc2` (`cc2`),
-  KEY `admin1` (`admin1`),
-  KEY `population` (`population`),
-  KEY `elevation` (`elevation`),
-  KEY `timezone` (`timezone`)
+  KEY `admin1` (`admin1`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- Create syntax for TABLE 'hierarchy'
@@ -136,10 +124,11 @@ CREATE TABLE `hierarchy` (
 
 -- Create syntax for TABLE 'iso_languagecodes'
 CREATE TABLE `iso_languagecodes` (
-  `iso_639_3` char(4) DEFAULT NULL,
+  `iso_639_3` char(4) NOT NULL,
   `iso_639_2` varchar(50) DEFAULT NULL,
   `iso_639_1` varchar(50) DEFAULT NULL,
-  `language_name` varchar(200) DEFAULT NULL
+  `language_name` tinytext NOT NULL,
+  PRIMARY KEY (`iso_639_3`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- Create syntax for TABLE 'timeZones'
@@ -152,8 +141,8 @@ CREATE TABLE `timeZones` (
 -- Create syntax for TABLE 'postalCodes'
 CREATE TABLE `postalCodes` (
   `country` char(2) DEFAULT NULL,
-  `postal_code` varchar(20) DEFAULT NULL,
-  `name` varchar(180) DEFAULT NULL,
+  `postal_code` varchar(16) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
   `admin1_name` varchar(100) DEFAULT NULL,
   `admin1_code` varchar(20) DEFAULT NULL,
   `admin2_name` varchar(100) DEFAULT NULL,
@@ -163,12 +152,34 @@ CREATE TABLE `postalCodes` (
   `latitude` decimal(10,7) DEFAULT NULL,
   `longitude` decimal(10,7) DEFAULT NULL,
   `accuracy` tinyint(2) DEFAULT NULL,
-  KEY `admin1_code` (`admin1_code`),
-  KEY `country` (`country`),
+  PRIMARY KEY (`country`, `postal_code`),
   KEY `name` (`name`),
   KEY `postal_code` (`postal_code`),
-  KEY `latitude` (`latitude`),
-  KEY `longitude` (`longitude`),
   KEY `admin1_name` (`admin1_name`),
   KEY `admin1_code_2` (`admin1_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE `admin1CodesAscii`
+ADD FOREIGN KEY (geonameid)
+REFERENCES geoname(geonameid);
+
+ALTER TABLE `admin2Codes`
+ADD FOREIGN KEY (geonameid)
+REFERENCES geoname(geonameid);
+
+ALTER TABLE `alternatename`
+ADD FOREIGN KEY (geonameid)
+REFERENCES geoname(geonameid);
+
+ALTER TABLE `continentCodes`
+ADD FOREIGN KEY (geonameid)
+REFERENCES geoname(geonameid);
+
+ALTER TABLE `countryinfo`
+ADD FOREIGN KEY (continent)
+REFERENCES continentCodes(code);
+
+ALTER TABLE `countryinfo`
+ADD FOREIGN KEY (geonameid)
+REFERENCES geoname(geonameid);
+
